f2874d69bfb7708cffeb4d218201bc5a820f6e36
Daniel Cesario <maneta@gmail.com>
2016-08-02 21:24:31 +0200

    Docker File | GemFile for dependencies | application changed to bind to 0.0.0.0

---
+++ app/hello-world.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'redis'
+
+# Listen on all interfaces
+set :bind, '0.0.0.0' 
+
+redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
+
+get '/' do
+  redis.ping
+  "Hello World!"
+end
+ 
