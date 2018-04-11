396e5dddd35be0e70ec04a340dcef7e62ade4dff
maneta <dcesario@redhat.com>
2018-04-11 19:09:42 +0200

    Corrections to the containers

--- app/hello-world.rb
+++ app/hello-world.rb
@@ -6,7 +6,7 @@ set :bind, '0.0.0.0'
 
 redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
 
-get '/user' do
+get '/' do
   redis.ping
 
   "Hello World!"
